41274132b61eebb107325c58d680d50b74758942
nikolayborzenko1331-jpg <nikolayborzenko1331@gmail.com>
2025-12-15 02:18:38 +0300

    Refactor static file serving and remove main route
    Removed the main route handler and updated comments.

--- server/server.js
+++ server/server.js
@@ -4,13 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* 🔥 ВАЖНО: раздаём статические файлы */
+/* Раздаём всё из public */
 app.use(express.static(path.join(__dirname, 'public')));
-
-/* Главная страница */
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log('DarkService backend running on port', PORT);
