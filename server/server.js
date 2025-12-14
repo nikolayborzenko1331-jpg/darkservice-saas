b30905e0b24aaf6f24d6d639c6d755c57cbb2bfe
nikolayborzenko1331-jpg <nikolayborzenko1331@gmail.com>
2025-12-15 02:22:39 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/* Раздаём всё из public */
-app.use(express.static(path.join(__dirname, 'public')));
+// 🔥 ВАЖНО: подключаем public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// 🔥 Главная страница
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// 🔥 Проверка, что сервер жив
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
 
 app.listen(PORT, () => {
   console.log('DarkService backend running on port', PORT);
