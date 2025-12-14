9724e57d48f5db0c1e222d188b24011dd328675f
nikolayborzenko1331-jpg <nikolayborzenko1331@gmail.com>
2025-12-15 01:27:55 +0300

    fix server listen port

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log('DarkService backend');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('DarkService backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
